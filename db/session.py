8b7b12b9a48efa17b4df2f87eb8f9eada5604722
hieuvu121 <hieubeobilabi@gmail.com>
2026-05-29 14:11:26 +1000

    fix null poll

--- db/session.py
+++ db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.pool import NullPool
 from config import settings
 
-engine = create_async_engine(settings.database_url, echo=False)
+engine = create_async_engine(settings.database_url, echo=False, poolclass=NullPool)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
